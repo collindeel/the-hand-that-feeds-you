2f216b301f047b7a950c51bc0b86d99de51e2dfd
Robert Locke <robertlockesoftware@gmail.com>
2025-05-09 16:10:19 -0700

    Add some terrain stuff

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEditor;
+
+public class TerrainGenerator : EditorWindow
+{
+    Terrain terrain;
+
+    [MenuItem("Tools/Generate Perlin Terrain")]
+    public static void ShowWindow()
+    {
+        GetWindow<TerrainGenerator>("Perlin Terrain Generator");
+    }
+
+    void OnGUI()
+    {
+        terrain = (Terrain)EditorGUILayout.ObjectField("Terrain", terrain, typeof(Terrain), true);
+
+        if (GUILayout.Button("Generate"))
+        {
+            if (terrain != null)
+            {
+                GenerateTerrain();
+            }
+        }
+    }
+
+    void GenerateTerrain()
+    {
+        int width = terrain.terrainData.heightmapResolution;
+        int height = terrain.terrainData.heightmapResolution;
+
+        float[,] heights = new float[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = (Mathf.PerlinNoise(x * 0.02f, y * 0.02f) - 0.5f) * 0.1f;
+
+            }
+        }
+
+        terrain.terrainData.SetHeights(0, 0, heights);
+    }
+}
