abf73c1d511c22ab2dc1efca935432361b404d1e
Mikayla <103014435+MikaSt23@users.noreply.github.com>
2025-05-12 22:34:50 -0700

    Rabbit Reaction
    When you feed the rabbits, heart particles appear around them

--- Assets/Scripts/RabbitReaction.cs
+++ Assets/Scripts/RabbitReaction.cs
@@ -3,31 +3,45 @@ using System.Collections;
 
 public class RabbitReaction : MonoBehaviour
 {
-    public Renderer rabbitRenderer;
-    public Color glowColor = Color.yellow;
-    public float glowDuration = 0.5f;
+    //public Renderer rabbitRenderer;
+    //public Color glowColor = Color.yellow;
+    //public float glowDuration = 0.5f;
+    public ParticleSystem heartParticles;
 
     private Color originalColor;
 
     void Start()
     {
-        if (rabbitRenderer == null)
+
+        if (heartParticles == null)
+        {
+            heartParticles = GetComponentInChildren<ParticleSystem>();
+        }
+
+
+        /*if (rabbitRenderer == null)
         {
             rabbitRenderer = GetComponentInChildren<Renderer>();
         }
 
         originalColor = rabbitRenderer.material.color;
+        */
     }
 
     public void ReactToFeeding()
     {
-        StartCoroutine(GlowRoutine());
+
+        if (heartParticles != null)
+        {
+            heartParticles.Play();
+        }
+        //StartCoroutine(GlowRoutine());
     }
 
-    IEnumerator GlowRoutine()
+    /*IEnumerator GlowRoutine()
     {
         rabbitRenderer.material.color = glowColor;
         yield return new WaitForSeconds(glowDuration);
         rabbitRenderer.material.color = originalColor;
-    }
+    }*/
 }
