c2cda269da2f90c2536866dd9e9681d331907c32
Collin Deel <collin.deel@gmail.com>
2025-05-31 13:44:33 -0700

    Add sprint and run keybindings to tutorial

--- Assets/Scripts/Objectives/Tutorial.cs
+++ Assets/Scripts/Objectives/Tutorial.cs
@@ -30,6 +30,7 @@ public class Tutorial : Objective
             ($"You must be {globalVaiables.playerName}-chan! I'm Unity-Chan. It's so nice to meet you!\n(* ^ w ^)", "Unity-Chan"),
             ("Since it's your first day on the job, I'll show you the ropes so we can get you ready!!", "Unity-Chan"),
             (@"If you see a carrot, you can pick it up by pressing <sprite name=""Interact"">. If you walk up to a bunny, you can press <sprite name=""Feed""> to feed it!", "Unity-Chan"),
+            (@"You can also use <sprite name=""Sprint""> to run and <sprite name=""Jump""> to jump!", "Unity-Chan"),
             ("Every time you feed a bunny, you'll earn points! By the end of the day, whatever your score is, I'll pay you that amount. (^v^)/ ", "Unity-Chan"),
             ("Be careful though, the bunnies can get a little feisty when they get hungry. (._.;)>", "Unity-Chan"),
             ($"Good luck {globalVaiables.playerName}, nyaa~! (^w^)", "Unity-Chan")
