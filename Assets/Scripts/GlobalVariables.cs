7ca88172b10bbc7989ec577295cf87ffc2c6a65e
Collin Deel <collin.deel@gmail.com>
2025-06-21 16:10:41 -0700

    Remove all files except leaderboard server

--- Assets/Scripts/GlobalVariables.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GlobalVariables : MonoBehaviour
-{
-    public bool showedContentWarning = false;
-    public bool gameCompleted = false;
-    public string playerName = "Misaki";
-    public bool isOverlay = false;
-}
--- Assets/Scripts/Objective.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class Objective : MonoBehaviour
-{
-    public abstract void Trigger();
-}
